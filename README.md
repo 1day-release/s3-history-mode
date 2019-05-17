a5cd47a8f8efe1902c6758d33b5206a14fd2bc66
Passionate Engineer <ryoju.builder@gmail.com>
2019-05-17 21:27:13 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# s3-history-mode
+
+## やりたいこと
+https://router.vuejs.org/ja/guide/essentials/history-mode.html
+
+ - /aboutにアクセスした時に、正常にページが読み込まれ「This is an about page」と表示されること
+ - URLパラメータを付与した時にも正常に遷移され、パラメータが維持されること(e.g. /about?test=test)
+
+## テスト用ファイル
+
+- /distにビルド済みのファイルを格納
---
+++ dist/precache-manifest.fd3fa2088440aa90777916a573f69345.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "95cea487880c5bb8ca31",
+    "url": "/js/about.1b91bce4.js"
+  },
+  {
+    "revision": "e04b12b5a302995b2b88",
+    "url": "/css/app.a56c603e.css"
+  },
+  {
+    "revision": "e04b12b5a302995b2b88",
+    "url": "/js/app.af142a89.js"
+  },
+  {
+    "revision": "dcbc0482898ccbe85c19",
+    "url": "/js/chunk-vendors.5d60a874.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d1c04419aecd2f2619f9560a11578934",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
